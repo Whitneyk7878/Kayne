9809026327f7b583468b015b6026a57a1d0ab082
Whitneyk7878 <whitneyk7878@my.uwstout.edu>
2025-02-08 23:39:46 -0600

    Update SetupPART2.sh

--- SetupPART2.sh
+++ SetupPART2.sh
@@ -498,8 +498,7 @@ systemctl disable netfs
 systemctl disable nfs
 sudo yum install lynis -y
 sudo auditctl -R /etc/audit/rules.d/audit.rules
-sudo systemctl status fail2ban
-sudo systemctl status auditd.service
-sudo systemctl status postfix
-sudo systemctl status dovecot
+sudo yum install ntpdate -y
+ntpdate pool.ntp.org
+#EXPIREMENTAL/////////////////////////////////////////////////////////////////////////////
 echo "FINISHED MAKE SURE YOU REBOOT"
