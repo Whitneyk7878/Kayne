a756a9aff5f5d8710f058a6434be2c4236577309
Whitneyk7878 <whitneyk7878@my.uwstout.edu>
2025-02-09 21:28:28 -0600

    Update SetupPART2.sh

--- SetupPART2.sh
+++ SetupPART2.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+sudo yum #!/bin/bash
 echo -e "starting script"
 
 
@@ -70,55 +70,55 @@ sudo systemctl enable iptables
 sudo systemctl start iptables
 
 # Empty all rules
-iptables -t filter -F
-iptables -t filter -X
+sudo iptables -t filter -F
+sudo iptables -t filter -X
 
 # Block everything by default
-iptables -t filter -P INPUT DROP
-iptables -t filter -P FORWARD DROP
-iptables -t filter -P OUTPUT DROP
+sudo iptables -t filter -P INPUT DROP
+sudo iptables -t filter -P FORWARD DROP
+sudo iptables -t filter -P OUTPUT DROP
 
 # Authorize already established connections
-iptables -A INPUT -m state --state RELATED,ESTABLISHED -j ACCEPT
-iptables -A OUTPUT -m state --state RELATED,ESTABLISHED -j ACCEPT
-iptables -t filter -A INPUT -i lo -j ACCEPT
-iptables -t filter -A OUTPUT -o lo -j ACCEPT
+sudo iptables -A INPUT -m state --state RELATED,ESTABLISHED -j ACCEPT
+sudo iptables -A OUTPUT -m state --state RELATED,ESTABLISHED -j ACCEPT
+sudo iptables -t filter -A INPUT -i lo -j ACCEPT
+sudo iptables -t filter -A OUTPUT -o lo -j ACCEPT
 
 # ICMP (Ping)
-iptables -t filter -A INPUT -p icmp -j ACCEPT
-iptables -t filter -A OUTPUT -p icmp -j ACCEPT
+sudo iptables -t filter -A INPUT -p icmp -j ACCEPT
+sudo iptables -t filter -A OUTPUT -p icmp -j ACCEPT
 
 # DNS (Needed for curl, and updates)
-iptables -t filter -A OUTPUT -p tcp --dport 53 -j ACCEPT
-iptables -t filter -A OUTPUT -p udp --dport 53 -j ACCEPT
+sudo iptables -t filter -A OUTPUT -p tcp --dport 53 -j ACCEPT
+sudo iptables -t filter -A OUTPUT -p udp --dport 53 -j ACCEPT
 
 # HTTP/HTTPS
-iptables -t filter -A OUTPUT -p tcp --dport 80 -j ACCEPT
-iptables -t filter -A OUTPUT -p tcp --dport 443 -j ACCEPT
+sudo iptables -t filter -A OUTPUT -p tcp --dport 80 -j ACCEPT
+sudo iptables -t filter -A OUTPUT -p tcp --dport 443 -j ACCEPT
 
 # NTP (server time)
-iptables -t filter -A OUTPUT -p udp --dport 123 -j ACCEPT
+sudo iptables -t filter -A OUTPUT -p udp --dport 123 -j ACCEPT
 
 # Splunk
-iptables -t filter -A OUTPUT -p tcp --dport 8000 -j ACCEPT
-iptables -t filter -A OUTPUT -p tcp --dport 8089 -j ACCEPT
-iptables -t filter -A OUTPUT -p tcp --dport 9997 -j ACCEPT
+sudo iptables -t filter -A OUTPUT -p tcp --dport 8000 -j ACCEPT
+sudo iptables -t filter -A OUTPUT -p tcp --dport 8089 -j ACCEPT
+sudo iptables -t filter -A OUTPUT -p tcp --dport 9997 -j ACCEPT
 
 # SMTP
-iptables -t filter -A OUTPUT -p tcp --dport 25 -j ACCEPT
-iptables -t filter -A INPUT -p tcp --dport 25 -j ACCEPT
+sudo iptables -t filter -A OUTPUT -p tcp --dport 25 -j ACCEPT
+sudo iptables -t filter -A INPUT -p tcp --dport 25 -j ACCEPT
 
 # POP3
-iptables -t filter -A OUTPUT -p tcp --dport 110 -j ACCEPT
-iptables -t filter -A INPUT -p tcp --dport 110 -j ACCEPT
+sudo iptables -t filter -A OUTPUT -p tcp --dport 110 -j ACCEPT
+sudo iptables -t filter -A INPUT -p tcp --dport 110 -j ACCEPT
+sudo iptables -t filter -A OUTPUT -p udp --dport 110 -j ACCEPT
+sudo iptables -t filter -A INPUT -p udp --dport 110 -j ACCEPT
 
 # IMAP
-iptables -t filter -A OUTPUT -p tcp --dport 143 -j ACCEPT
-iptables -t filter -A INPUT -p tcp --dport 143 -j ACCEPT
-
-#make sure only one firewall installed
-sudo systemctl stop firewalld
-sudo systemctl disable firewalld
+sudo iptables -t filter -A OUTPUT -p tcp --dport 143 -j ACCEPT
+sudo iptables -t filter -A INPUT -p tcp --dport 143 -j ACCEPT
+sudo iptables -t filter -A OUTPUT -p udp --dport 143 -j ACCEPT
+sudo iptables -t filter -A INPUT -p udp --dport 143 -j ACCEPT
 
 
 echo -e "\e[38;5;46m//////////////////////////////////////////////////////\e[0m"
