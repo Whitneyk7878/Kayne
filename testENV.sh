9f98e7cda57bea7b72e2b2ffca88c011603a1cc3
Whitneyk7878 <whitneyk7878@my.uwstout.edu>
2025-02-12 20:45:56 -0600

    Update testENV.sh

--- testENV.sh
+++ testENV.sh
@@ -1,10 +1,1 @@
-#!/bin/bash
-sed -i '/\[dovecot\]/a enabled = true\nmaxretry = 5\nbantime = 3600' /etc/fail2ban/jail.local
-#FOR THE COMPETITION
-# Apache Stuff
-echo "Making an Apache jail..."
-sed -i '/\[apache-auth\]/a enabled = true\nmaxretry = 5\nbantime = 3600' /etc/fail2ban/jail.local
-# Roundcube Stuff
-echo "Making an Roundcube jail..."
-sed -i '/\[roundcube-auth\]/a enabled = true\nmaxretry = 5\nbantime = 3600' /etc/fail2ban/jail.local
-
+sudo wget https://github.com/activecm/rita/releases/download/v5.0.8/install-rita-zeek-here.sh
