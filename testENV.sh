90f434d4c3e9c4236254aaaa5073a4fec530e214
Whitneyk7878 <whitneyk7878@my.uwstout.edu>
2025-02-12 19:54:41 -0600

    Update testENV.sh

--- testENV.sh
+++ testENV.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-sed -i '/^\[apache-auth\]/,/^$/s/^bantime.*/bantime = 3600/' /etc/fail2ban/jail.local
-sed -i '/^\[apache-auth\]/,/^$/s/^maxretry.*/maxretry = 5/' /etc/fail2ban/jail.local
-sed -i '/^\[apache-auth\]/,/^$/s|^logpath.*|logpath = /var/log/fail2banlog|' /etc/fail2ban/jail.local
+sed -i '/\[dovecot\]/a enabled = true\nmaxretry = 5\nbantime = 3600' /etc/fail2ban/jail.local
+sed -i 's|logpath = %(dovecot_log)s|logpath = /var/log/fail2banlog|g' /etc/fail2ban/jail.local
+#FOR THE COMPETITION
+# Apache Stuff
+echo "Making an Apache jail..."
+sed -i '/\[apache-auth\]/a enabled = true\nmaxretry = 5\nbantime = 3600' /etc/fail2ban/jail.local
+sed -i 's|logpath = %(apache_error_log)s|logpath = /var/log/fail2banlog|g' /etc/fail2ban/jail.local
 
