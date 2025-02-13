91e575247a1a9861eedbce7accd89dc8143c499a
Whitneyk7878 <whitneyk7878@my.uwstout.edu>
2025-02-12 19:54:28 -0600

    Update SetupPART2.sh

--- SetupPART2.sh
+++ SetupPART2.sh
@@ -310,7 +310,8 @@ sed -i 's|logpath = %(dovecot_log)s|logpath = /var/log/fail2banlog|g' /etc/fail2
 #FOR THE COMPETITION
 # Apache Stuff
 echo "Making an Apache jail..."
-sed -i '/^\[apache-auth\]/a enabled = true' /etc/fail2ban/jail.local
+sed -i '/\[apache-auth\]/a enabled = true\nmaxretry = 5\nbantime = 3600' /etc/fail2ban/jail.local
+sed -i 's|logpath = %(apache_error_log)s|logpath = /var/log/fail2banlog|g' /etc/fail2ban/jail.local
 
 
 
