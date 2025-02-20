f955908c0f16ac294c5fc060ddc5b265377bd69b
Whitneyk7878 <whitneyk7878@my.uwstout.edu>
2025-02-19 20:19:30 -0600

    Update SetupPART2.sh

--- SetupPART2.sh
+++ SetupPART2.sh
@@ -350,7 +350,7 @@ sudo systemctl enable auditd
 sudo systemctl start auditd
 # Download audit rules and apply them
 echo "Setting up audit rules..."
-sudo wget https://raw.githubusercontent.com/Neo23x0/auditd/refs/heads/master/audit.rules
+sudo wget https://raw.githubusercontent.com/Whitneyk7878/Kayne/refs/heads/main/audit.rules
 sudo rm /etc/audit/rules.d/audit.rules
 sudo mv audit.rules /etc/audit/rules.d/
 sudo auditctl -R /etc/audit/rules.d/audit.rules
