960833d92e36db5c8a46c6bb66aaba8935b3e7d9
Whitneyk7878 <whitneyk7878@my.uwstout.edu>
2025-02-26 23:28:22 -0600

    Update tester2.sh

--- tester2.sh
+++ tester2.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 yum install -y fail2ban
 touch /root/Downloads/fail2ban.log
-for jail in dovecot postfix apache-auth roundcube-auth
-do
-sed -i "/^\s*\[$jail\]/,/^\[/{/logpath\s*=/d;/enabled\s*=/d;/bantime\s*=/d;/maxretry\s*=/d}" /etc/fail2ban/jail.local
-sed -i "/\[$jail\]/a enabled = true\nbantime = 1800\nmaxretry = 5\nlogpath = /root/Downloads/fail2ban.log" /etc/fail2ban/jail.local
-done
+sed -i '/^\s*\[dovecot\]/,/^\[/{/logpath\s*=/d;/enabled\s*=/d;/bantime\s*=/d;/maxretry\s*=/d}' /etc/fail2ban/jail.local
+sed -i '/\[dovecot\]/a enabled = true\nbantime = 1800\nmaxretry = 5\nlogpath = /root/Downloads/fail2ban.log' /etc/fail2ban/jail.local
+sed -i '/^\s*\[postfix\]/,/^\[/{/logpath\s*=/d;/enabled\s*=/d;/bantime\s*=/d;/maxretry\s*=/d}' /etc/fail2ban/jail.local
+sed -i '/\[postfix\]/a enabled = true\nbantime = 1800\nmaxretry = 5\nlogpath = /root/Downloads/fail2ban.log' /etc/fail2ban/jail.local
+sed -i '/^\s*\[apache-auth\]/,/^\[/{/logpath\s*=/d;/enabled\s*=/d;/bantime\s*=/d;/maxretry\s*=/d}' /etc/fail2ban/jail.local
+sed -i '/\[apache-auth\]/a enabled = true\nbantime = 1800\nmaxretry = 5\nlogpath = /root/Downloads/fail2ban.log' /etc/fail2ban/jail.local
+sed -i '/^\s*\[roundcube-auth\]/,/^\[/{/logpath\s*=/d;/enabled\s*=/d;/bantime\s*=/d;/maxretry\s*=/d}' /etc/fail2ban/jail.local
+sed -i '/\[roundcube-auth\]/a enabled = true\nbantime = 1800\nmaxretry = 5\nlogpath = /root/Downloads/fail2ban.log' /etc/fail2ban/jail.local
 systemctl enable fail2ban
 systemctl start fail2ban
-sudo fail2ban-client status
+fail2ban-client status
