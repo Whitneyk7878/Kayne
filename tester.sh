de6c6c968966adedf963bfaa2e7808652f670f4e
Whitneyk7878 <whitneyk7878@my.uwstout.edu>
2025-02-26 22:48:39 -0600

    Update tester.sh

--- tester.sh
+++ tester.sh
@@ -4,9 +4,9 @@ sed -i -e '/\[apache-auth\]/,/^\[/{/enabled\s*=/d;/bantime\s*=/d;/maxretry\s*=/d
        -e '/\[apache-auth\]/a enabled = true\nbantime = 3600\nmaxretry = 5' \
        /etc/fail2ban/jail.local
 
-sed -i -e '/\[roundcube-auth\]/,/^\[/{/enabled\s*=/d;/bantime\s*=/d;/maxretry\s*=/d}' \
-       -e '/\[roundcube-auth\]/a enabled = true\nbantime = 3600\nmaxretry = 5' \
-       /etc/fail2ban/jail.local
+#sed -i -e '/\[roundcube-auth\]/,/^\[/{/enabled\s*=/d;/bantime\s*=/d;/maxretry\s*=/d}' \
+#       -e '/\[roundcube-auth\]/a enabled = true\nbantime = 3600\nmaxretry = 5' \
+#       /etc/fail2ban/jail.local
 
 sed -i -e '/\[dovecot\]/,/^\[/{/enabled\s*=/d;/bantime\s*=/d;/maxretry\s*=/d}' \
        -e '/\[dovecot\]/a enabled = true\nbantime = 3600\nmaxretry = 5' \
