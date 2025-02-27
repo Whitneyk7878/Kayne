6c35663d113c941de3482847cb7e5ff93b6b36db
Whitneyk7878 <whitneyk7878@my.uwstout.edu>
2025-02-27 00:03:08 -0600

    Update tester2.sh

--- tester2.sh
+++ tester2.sh
@@ -1,13 +1,2 @@
 #!/bin/bash
-yum install -y fail2ban
-touch /root/Downloads/fail2ban.log
-sed -i '/^\s*\[dovecot\]/,/^\[/{/logpath\s*=/d;/enabled\s*=/d;/bantime\s*=/d;/maxretry\s*=/d}' /etc/fail2ban/jail.conf
-sed -i '/\[dovecot\]/a enabled = true\nbantime = 1800\nmaxretry = 5\nlogpath = /root/Downloads/fail2ban.log' /etc/fail2ban/jail.conf
-sed -i '/^\s*\[postfix\]/,/^\[/{/logpath\s*=/d;/enabled\s*=/d;/bantime\s*=/d;/maxretry\s*=/d}' /etc/fail2ban/jail.conf
-sed -i '/\[postfix\]/a enabled = true\nbantime = 1800\nmaxretry = 5\nlogpath = /root/Downloads/fail2ban.log' /etc/fail2ban/jail.conf
-sed -i '/^\s*\[apache-auth\]/,/^\[/{/logpath\s*=/d;/enabled\s*=/d;/bantime\s*=/d;/maxretry\s*=/d}' /etc/fail2ban/jail.conf
-sed -i '/\[apache-auth\]/a enabled = true\nbantime = 1800\nmaxretry = 5\nlogpath = /root/Downloads/fail2ban.log' /etc/fail2ban/jail.conf
-sed -i '/^\s*\[roundcube-auth\]/,/^\[/{/logpath\s*=/d;/enabled\s*=/d;/bantime\s*=/d;/maxretry\s*=/d}' /etc/fail2ban/jail.conf
-sed -i '/\[roundcube-auth\]/a enabled = true\nbantime = 1800\nmaxretry = 5\nlogpath = /root/Downloads/fail2ban.log' /etc/fail2ban/jail.conf
-systemctl enable fail2ban
-systemctl start fail2ban
+sed -i -e '/^[;\s]*open_basedir\s*=/d' -e '$ a open_basedir = "/usr/share/roundcubemail/:/var/lib/roundcube/:/tmp/"' /etc/php.ini
