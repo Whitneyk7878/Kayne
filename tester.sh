72bd6cf6090225651c33daeb0f1321509f33d61f
Whitneyk7878 <whitneyk7878@my.uwstout.edu>
2025-02-26 22:50:58 -0600

    Update tester.sh

--- tester.sh
+++ tester.sh
@@ -12,7 +12,7 @@ sed -i -e '/\[dovecot\]/,/^\[/{/enabled\s*=/d;/bantime\s*=/d;/maxretry\s*=/d}' \
        -e '/\[dovecot\]/a enabled = true\nbantime = 3600\nmaxretry = 5' \
        /etc/fail2ban/jail.local
 
-sed -i -e '/\[postfix\]/,/^\[/{/enabled\s*=/d;/bantime\s*=/d;/maxretry\s*=/d}' \
-       -e '/\[postfix\]/a enabled = true\nbantime = 3600\nmaxretry = 5' \
-       /etc/fail2ban/jail.local
+#sed -i -e '/\[postfix\]/,/^\[/{/enabled\s*=/d;/bantime\s*=/d;/maxretry\s*=/d}' \
+#       -e '/\[postfix\]/a enabled = true\nbantime = 3600\nmaxretry = 5' \
+#       /etc/fail2ban/jail.local
 
