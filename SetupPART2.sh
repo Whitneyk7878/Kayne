ea768b895a45cd3c3cc4e578676699eee3623e57
Whitneyk7878 <whitneyk7878@my.uwstout.edu>
2025-02-27 00:45:07 -0600

    Update SetupPART2.sh

--- SetupPART2.sh
+++ SetupPART2.sh
@@ -685,6 +685,39 @@ sudo mv /usr/bin/gdb     /usr/bin/oldgdb
 sudo mv /usr/bin/strace  /usr/bin/oldstrace  
 sudo mv /usr/bin/ltrace  /usr/bin/oldltrace  
 
+echo -e "\e[38;5;46m//////////////////////////////////////////////////////\e[0m"
+echo -e "\e[38;5;46m              Locking Down Critical Files             \e[0m"
+echo -e "\e[38;5;46m//////////////////////////////////////////////////////\e[0m"
+sleep 1
+#!/bin/bash
+# List of critical files to protect
+FILES=(
+    /etc/passwd
+    /etc/shadow
+    /etc/group
+    /etc/gshadow
+    /etc/sudoers
+    /etc/ssh/sshd_config
+    /etc/ssh/ssh_config
+    /etc/crontab
+    /etc/fstab
+    /etc/hosts
+    /etc/resolv.conf
+    /etc/sysctl.conf
+    /etc/selinux/config
+)
+
+# Loop through each file and set it immutable if it exists
+for file in "${FILES[@]}"; do
+    if [ -f "$file" ]; then
+        chattr +i "$file"
+        echo "Set immutable on $file"
+    else
+        echo "File not found: $file"
+    fi
+done
+
+
 
 echo -e "\e[38;5;46m//////////////////////////////////////////////////////\e[0m"
 echo -e "\e[38;5;46m            Initializing AIDE Database                \e[0m"
