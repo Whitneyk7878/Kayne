2d6f3fe1a7449c24e9468eb0f6a71e51c386676c
Whitneyk7878 <whitneyk7878@my.uwstout.edu>
2025-02-26 22:54:41 -0600

    Update tester.sh

--- tester.sh
+++ tester.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-sed -i -e '/\[apache-auth\]/,/^\[/{/enabled\s*=/d;/bantime\s*=/d;/maxretry\s*=/d}' \
-       -e '/\[apache-auth\]/a enabled = true\nbantime = 3600\nmaxretry = 5' \
-       /etc/fail2ban/jail.local
+#sed -i -e '/\[apache-auth\]/,/^\[/{/enabled\s*=/d;/bantime\s*=/d;/maxretry\s*=/d}' \
+#       -e '/\[apache-auth\]/a enabled = true\nbantime = 3600\nmaxretry = 5' \
+#       /etc/fail2ban/jail.local
 
 #sed -i -e '/\[roundcube-auth\]/,/^\[/{/enabled\s*=/d;/bantime\s*=/d;/maxretry\s*=/d}' \
 #       -e '/\[roundcube-auth\]/a enabled = true\nbantime = 3600\nmaxretry = 5' \
