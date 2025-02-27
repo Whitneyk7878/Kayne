6caa1a892ea90b62a2f7d1155f03581985154cf0
Whitneyk7878 <whitneyk7878@my.uwstout.edu>
2025-02-26 22:57:57 -0600

    Update tester.sh

--- tester.sh
+++ tester.sh
@@ -1,18 +1,9 @@
 #!/bin/bash
 
-#sed -i -e '/\[apache-auth\]/,/^\[/{/enabled\s*=/d;/bantime\s*=/d;/maxretry\s*=/d}' \
-#       -e '/\[apache-auth\]/a enabled = true\nbantime = 3600\nmaxretry = 5' \
-#       /etc/fail2ban/jail.local
+sed -i '/\[apache-auth\]/,/^\[/{/enabled\s*=/d;/bantime\s*=/d;/maxretry\s*=/d};/\[apache-auth\]/a enabled = true\nbantime = 3600\nmaxretry = 5' /etc/fail2ban/jail.local
 
-#sed -i -e '/\[roundcube-auth\]/,/^\[/{/enabled\s*=/d;/bantime\s*=/d;/maxretry\s*=/d}' \
-#       -e '/\[roundcube-auth\]/a enabled = true\nbantime = 3600\nmaxretry = 5' \
-#       /etc/fail2ban/jail.local
+sed -i '/\[roundcube-auth\]/,/^\[/{/enabled\s*=/d};/\[roundcube-auth\]/a enabled = true' /etc/fail2ban/jail.local
 
-sed -i -e '/\[dovecot\]/,/^\[/{/enabled\s*=/d;/bantime\s*=/d;/maxretry\s*=/d}' \
-       -e '/\[dovecot\]/a enabled = true\nbantime = 3600\nmaxretry = 5' \
-       /etc/fail2ban/jail.local
+sed -i '/\[dovecot\]/,/^\[/{/enabled\s*=/d;/bantime\s*=/d;/maxretry\s*=/d};/\[dovecot\]/a enabled = true\nbantime = 3600\nmaxretry = 5' /etc/fail2ban/jail.local
 
-#sed -i -e '/\[postfix\]/,/^\[/{/enabled\s*=/d;/bantime\s*=/d;/maxretry\s*=/d}' \
-#       -e '/\[postfix\]/a enabled = true\nbantime = 3600\nmaxretry = 5' \
-#       /etc/fail2ban/jail.local
-
+sed -i '/\[postfix\]/,/^\[/{/enabled\s*=/d;/bantime\s*=/d;/maxretry\s*=/d};/\[postfix\]/a enabled = true\nbantime = 3600\nmaxretry = 5' /etc/fail2ban/jail.local
