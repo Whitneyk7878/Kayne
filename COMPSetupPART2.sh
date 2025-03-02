ee20e85ae4b1765bb05f0b3ac2641f5036f0518d
Whitneyk7878 <whitneyk7878@my.uwstout.edu>
2025-03-02 01:38:42 -0600

    Update COMPSetupPART2.sh

--- COMPSetupPART2.sh
+++ COMPSetupPART2.sh
@@ -371,6 +371,27 @@ echo -e "\e[38;5;46m//////////////////////////////////////////////////////\e[0m"
 #postconf -e 'smtpd_tls_loglevel = 1'
 #sudo systemctl restart postfix
 
+echo "Configuring Postfix..."
+POSTFIX_CONFIG="/etc/postfix/main.cf"
+
+declare -A POSTFIX_SETTINGS=(
+    ["smtpd_client_connection_count_limit"]="10"
+    ["smtpd_client_connection_rate_limit"]="60"
+    ["smtpd_error_sleep_time"]="5s"
+    ["smtpd_soft_error_limit"]="10"
+    ["smtpd_hard_error_limit"]="20"
+    ["message_size_limit"]="10485760"
+    ["smtpd_recipient_restrictions"]="reject_unauth_destination"
+)
+
+for key in "${!POSTFIX_SETTINGS[@]}"; do
+    if ! grep -q "^$key" "$POSTFIX_CONFIG"; then
+        echo "$key = ${POSTFIX_SETTINGS[$key]}" >> "$POSTFIX_CONFIG"
+    fi
+done
+
+sudo systemctl restart postfix
+
 echo -e "\e[38;5;46m//////////////////////////////////////////////////////\e[0m"
 echo -e "\e[38;5;46m                Implementing Fail2Ban                 \e[0m"
 echo -e "\e[38;5;46m//////////////////////////////////////////////////////\e[0m"
