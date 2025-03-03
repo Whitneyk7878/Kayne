e0f0146c54fefb4ac551c875443c80ffb9c0acb5
Whitneyk7878 <whitneyk7878@my.uwstout.edu>
2025-03-02 21:00:03 -0600

    Update COMPSetupPART2.sh

--- COMPSetupPART2.sh
+++ COMPSetupPART2.sh
@@ -8,7 +8,7 @@
 
 
 echo -e "\e[38;5;46m//////////////////////////////////////////////////////\e[0m"
-echo -e "\e[38;5;46m             General Security Measures                      \e[0m"
+echo -e "\e[38;5;46m             General Security Measures                \e[0m"
 echo -e "\e[38;5;46m//////////////////////////////////////////////////////\e[0m"
 sleep 1
 # Only allow root login from console
@@ -88,6 +88,33 @@ echo "Restarting fail2ban service..."
 systemctl enable fail2ban
 systemctl restart fail2ban
 
+
+echo -e "\e[38;5;46m//////////////////////////////////////////////////////\e[0m"
+echo -e "\e[38;5;46m         Installing Comp Tools from Github            \e[0m"
+echo -e "\e[38;5;46m//////////////////////////////////////////////////////\e[0m"
+sleep 1
+# Create the directory if it doesn't exist
+mkdir -p COMPtools
+
+# Base URL for the files
+base_url="https://raw.githubusercontent.com/Whitneyk7878/Kayne/refs/heads/main/"
+
+# List of files to download
+files=(
+    "COMPMailBoxClear.sh"
+    "COMPInstallBroZEEK.sh"
+    "COMPBackupFIREWALL.sh"
+    "COMPcreatebackups.sh"
+    "COMPrestorefrombackup.sh"
+)
+
+# Loop over each file and download it into the COMPtools directory
+for file in "${files[@]}"; do
+    echo "Downloading ${file}..."
+    wget -P COMPtools "${base_url}${file}"
+done
+
+echo "All files have been downloaded to the COMPtools directory."
 
 
 
