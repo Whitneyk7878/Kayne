5cc8c1ab6de4fc4cae4dd89422fd335c84c8852e
Whitneyk7878 <whitneyk7878@my.uwstout.edu>
2025-03-06 22:55:44 -0600

    Update tester.sh

--- tester.sh
+++ tester.sh
@@ -33,29 +33,29 @@ mkdir -p "$BACKUP_DIR1" "$BACKUP_DIR2"
 echo "Starting backup..."
 
 # Backup Apache (configuration and web files)
-zip -r "$BACKUP_DIR1/apache.zip" /etc/httpd /var/www
-cp "$BACKUP_DIR1/apache.zip" "$BACKUP_DIR2/"
-echo "Apache backup complete."
+#zip -r "$BACKUP_DIR1/apache.zip" /etc/httpd /var/www
+#cp "$BACKUP_DIR1/apache.zip" "$BACKUP_DIR2/"
+#echo "Apache backup complete."
 
 # Backup Postfix (configuration)
-zip -r "$BACKUP_DIR1/postfix.zip" /etc/postfix
-cp "$BACKUP_DIR1/postfix.zip" "$BACKUP_DIR2/"
-echo "Postfix backup complete."
+#zip -r "$BACKUP_DIR1/postfix.zip" /etc/postfix
+#cp "$BACKUP_DIR1/postfix.zip" "$BACKUP_DIR2/"
+#echo "Postfix backup complete."
 
 # Backup Dovecot (configuration)
-zip -r "$BACKUP_DIR1/dovecot.zip" /etc/dovecot
-cp "$BACKUP_DIR1/dovecot.zip" "$BACKUP_DIR2/"
-echo "Dovecot backup complete."
+#zip -r "$BACKUP_DIR1/dovecot.zip" /etc/dovecot
+#cp "$BACKUP_DIR1/dovecot.zip" "$BACKUP_DIR2/"
+#echo "Dovecot backup complete."
 
 # Backup Roundcube (configuration and web files)
-zip -r "$BACKUP_DIR1/roundcubemail.zip" /etc/roundcubemail /usr/share/roundcubemail
-cp "$BACKUP_DIR1/roundcubemail.zip" "$BACKUP_DIR2/"
-echo "Roundcubemail backup complete."
+#zip -r "$BACKUP_DIR1/roundcubemail.zip" /etc/roundcubemail /usr/share/roundcubemail
+#cp "$BACKUP_DIR1/roundcubemail.zip" "$BACKUP_DIR2/"
+#echo "Roundcubemail backup complete."
 
 # Backup MariaDB (data directory)
-zip -r "$BACKUP_DIR1/mariadb.zip" /var/lib/mysql
-cp "$BACKUP_DIR1/mariadb.zip" "$BACKUP_DIR2/"
-echo "MariaDB backup complete."
+#zip -r "$BACKUP_DIR1/mariadb.zip" /var/lib/mysql
+#cp "$BACKUP_DIR1/mariadb.zip" "$BACKUP_DIR2/"
+#echo "MariaDB backup complete."
 
 # Backup network configurations
 zip -r "$BACKUP_DIR1/network_configs.zip" /etc/sysconfig/network /etc/sysconfig/network-scripts
